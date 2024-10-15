e7cd708a2c36b21d72c82ca82b7e4511484660ff
ayaalshouha <ayaalshouha12@gmail.com>
2024-10-15 15:05:15 +0300

    build a custom validator

--- src/app/auth/login-reactive/login-reactive.component.ts
+++ src/app/auth/login-reactive/login-reactive.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return { doesNotContainQuestionMark: true };
+}
 
 @Component({
   selector: 'app-login-reactive',
@@ -20,7 +29,11 @@ export class LoginReactiveComponent {
       validators: [Validators.email, Validators.required],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
     }),
   });
 
