c09012783278e10de03dcec2e4f9344b8764d3a8
ayaalshouha <ayaalshouha12@gmail.com>
2024-10-15 13:58:59 +0300

    add login reactive form component

---
+++ src/app/auth/login-reactive/login-reactive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-reactive',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-reactive.component.html',
+  styleUrl: './login-reactive.component.css'
+})
+export class LoginReactiveComponent {
+
+}
