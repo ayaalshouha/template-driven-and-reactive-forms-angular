76a39e53ff550cf314b8ae7abd2b8406d5e8d5b9
ayaalshouha <ayaalshouha12@gmail.com>
2024-10-15 18:37:42 +0300

    add asyncValidators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,19 +1,49 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { of } from 'rxjs';
 
+function isEmailUnique(control: AbstractControl) {
+  if (control.value !== 'admin@example.com') {
+    return of(null);
+  }
+  return of({ notUnique: true });
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
 export class SignupComponent {
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
+      asyncValidators: [isEmailUnique],
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)],
     }),
   });
+
+  get InvalidEmail() {
+    return (
+      this.form.controls['email'].invalid &&
+      this.form.controls['email'].touched &&
+      this.form.controls['email'].dirty
+    );
+  }
+  onSubmit() {
+    console.log(this.form);
+  }
+
+  onReset() {
+    this.form.reset();
+  }
 }
