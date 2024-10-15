8c93517c099f37f4a6529479abd28c7be9c75f31
ayaalshouha <ayaalshouha12@gmail.com>
2024-10-15 14:24:13 +0300

    FormGroup and FormControl

--- src/app/auth/login-reactive/login-reactive.component.ts
+++ src/app/auth/login-reactive/login-reactive.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-reactive',
   standalone: true,
   imports: [],
   templateUrl: './login-reactive.component.html',
-  styleUrl: './login-reactive.component.css'
+  styleUrl: './login-reactive.component.css',
 })
 export class LoginReactiveComponent {
-
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 }
